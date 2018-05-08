a5d70d4a1233b66e17bfcfe04d96b55a442cedc8
Sainathhiwale <sainathhiwale2@gmail.com>
2018-05-09 04:36:09 +0530

    design updated

---
+++ sainathhiwale/src/components/Expertise.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-container fluid class="pa-0">
+    <v-layout row wrap>
+      <v-flex md6 sm12>
+        <div class="text-xs-center">
+          <v-chip v-for="skill in skills" :key="skill.name">
+            <v-avatar class="teal">{{skill.type}}</v-avatar>
+            {{skill.name}}
+          </v-chip>
+        </div>
+      </v-flex>
+       <v-flex md6 sm12>
+        <div class="text-xs-right">
+         <v-chip class="teal">W - Web</v-chip>
+        <v-chip class="teal">B - Backend</v-chip>
+         <v-chip class="teal">M - Mobile</v-chip>
+          <v-chip class="teal">L - Language</v-chip>
+        <v-chip class="teal">C - Cloud</v-chip>
+         <v-chip class="teal">O - Others</v-chip>
+        </div>
+      </v-flex>
+    </v-layout>
+     <p class="mt-4">
+      <a href="https://www.linkedin.com/in/vidyasagarmsc" target="_blank">Other Featured Skills & Endorsements</a>
+    </p>
+  </v-container>
+  
+</template>
+
+<script>
+export default {
+  name: 'expertise',
+  data: () => {
+    return {
+      skills: [
+        { type: 'L', name: 'C#' },
+        { type: 'L', name: 'Javascript' },
+       { type: 'L', name: 'Swift' },
+       { type: 'W', name: 'HTML' },
+        { type: 'W', name: 'CSS' },
+       { type: 'M', name: 'Android' },
+       { type: 'M', name: 'iOS' },
+        { type: 'M', name: 'Ionic' },
+       { type: 'B', name: 'NoSQL' },
+       { type: 'C', name: 'IBM Cloud' },
+        { type: 'C', name: 'Google Cloud Platform' },
+       { type: 'C', name: 'Microsoft Azure' },
+       { type: 'W', name: 'AngularJS' },
+        { type: 'W', name: 'VueJS' },
+       { type: 'B', name: 'SQL Server' },
+       { type: 'O', name: 'Unity 3D' },
+        { type: 'O', name: 'Machine Intelligence' }
+
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+h4 {
+  font-weight: bold;
+}
+.language-container {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  flex-direction: column;
+}
+.blue{
+  color: #66c0ec;
+}
+</style>
--- sainathhiwale/src/components/Home.vue
+++ sainathhiwale/src/components/Home.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class="hello">
+  <div class="home">
     <img src="/static/images/sainath.jpg"/>
+    <h1 class="inline">Sainath Hiwale</h1>
+    <h3 class="inline">Coder | Singer | Explorer</h3>
+    <p v-html ="about"></p>
+    <p v-html = "more"></p>
+    <p v-html ="possinate"></p>
+   <v-divider></v-divider>
+    <h3 class="inline">Accomplishments</h3>
+    <p>
+      <ul>
+        <li>March 2018 | <strong>Google Play Store</strong> Examen App</li><br>
+        <li>May 2018 | <strong>Firebase</strong> ExamenProgramming Site</li><br>
+      </ul>
+    </p>
+   <v-divider></v-divider>
+    <h3 class="inline">Recommendations</h3>
+    <carousel></carousel>
+    <v-divider></v-divider>
+    <h3 class="inline">Skill Set</h3>
+    <expertise></expertise>
     <h1>{{ msg }}</h1>
     <h2>Sainath Hiwale</h2>
     
@@ -8,17 +27,24 @@
 </template>
 
 <script>
+import Expertise from './Expertise'  
+import Carousel from './Carousel'
 export default {
-  name: 'HelloWorld',
+  name: 'home',
   data () {
     return {
-      msg: 'My Personal Website'
+      msg: 'Sainath Hiwale',
+      about: 'Sainath is a passionate and pragmatic programmer,I love to learn new technology <a href="/writes" style="color: #EA8E28 !important;" target="_blank">blogger</a> and also <a href="/engagements" style="color: #EA8E28 !important;" target="_blank">speaker</a> Programming tutorial coding <a href="/projects" style="color: #EA8E28 !important;" target="_blank"> samples</a>, provides to you.',
+      more: 'Sainath has 1 year Experience in Android development.I have just Published my own app Google Play Store <a href="https://play.google.com/store/apps/details?id=com.dorvis.quizapp" style="color: #EA8E28 !important;" target="_blank">Examen App</a> <br>Examen is an site where you can learn new Programming language as well as some tutorial and code sample.<a href="https://examenprogramming.firebaseapp.com/" style="color: #EA8E28 !important;" target="_blank">ExamenProgramming</a><br>I have done an MCA from Sinhgad Institute of Business Administration & Computer Application at Savitribai Phule Pune University.'
+
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+@import url('https://fonts.googleapis.com/css?family=Quattrocento+Sans:400,700');
+@import url('https://fonts.googleapis.com/css?family=Open+Sans');
+@import url('https://fonts.googleapis.com/css?family=Poppins');
 <style scoped>
 h1, h2 {
   font-weight: normal;
@@ -32,6 +58,40 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #F0FFFF;
+ font-weight: bold;
+ -webkit-transition: color 0.2s ease-in-out;
+-moz-transition: color 0.2s ease-in-out;
+-o-transition: color 0.2s ease-in-out;
+transition: color 0.2s
+}
+p{
+font-family: 'Poppins', sans-serif;
+  position: relative;
+  padding-left: 0.9375rem;
+  padding-right: 0.9375rem;
+  font-size: 18px;
+  line-height: 2;
+  color: #F0F8FF;
+  text-rendering: optimizeLegibility;
+
+}
+.home img{
+padding: 1rem 0;
+margin: 3rem 0;
+width: 300px;
+border-radius: 50%;
+margin: 0 auto;
+display: table
+}
+h4{
+font-size:18px;
+font-weight: bold;
+}
+.inline{
+color:#66c0ec !important;
+}
+.background{
+  background-color: #fff;
 }
 </style>
